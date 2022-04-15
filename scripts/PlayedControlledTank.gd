507c7fc9a61018353ca4eae8c40cc2e261140245
Jerry <47512608+Jerry-Licious@users.noreply.github.com>
2022-04-14 20:37:50 -0400

    The player tank is now called "Player".

--- scripts/PlayedControlledTank.gd
+++ scripts/PlayedControlledTank.gd
@@ -3,7 +3,7 @@ extends "res://scripts/Tank.gd"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Nothing happening here yet.
+	set_name("Player")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
